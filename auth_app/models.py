cfbe9ead45dc460f2de83825a249ba0d1fd9d6b3
pantech48 <evgeniy.putrin@gmail.com>
2023-03-13 16:36:45 +0100

    * login user func reworked

--- auth_app/models.py
+++ auth_app/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.dialects.postgresql import UUID
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
--- auth_app/user.py
+++ auth_app/user.py
@@ -1,12 +1,18 @@
+from urllib import request
 from . import schemas, models
 from sqlalchemy.orm import Session
-from fastapi import Depends, HTTPException, status, APIRouter, Response, FastAPI
+from fastapi import Depends, HTTPException, status, APIRouter, Response, FastAPI, Request
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import jwt
+import datetime
 
 from .database import get_db
 from auth_app.utils import hash_sha256_password
+from config.config_parser import config
 
 
 app = FastAPI()
+security = HTTPBasic()
 
 
 @app.post('/user')
@@ -18,7 +24,7 @@ async def create_user(payload: schemas.User, db: Session = Depends(get_db)):
         db.add(user)
         db.commit()
         db.refresh(user)
-        return {"username": user.username, "email": user.email}
+        return {"uuid": user.id, "username": user.username, "email": user.email}
     except Exception:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"User with username {payload.username} already exists")
@@ -52,12 +58,15 @@ async def delete_user(payload: schemas.User, db: Session = Depends(get_db)):
                             detail=f"User with username {payload.username} does not exist")
 
 
-@app.get('/user')
-async def login(payload: schemas.User, db: Session = Depends(get_db)):
+@app.get('/login')
+async def login(payload: schemas.User, credentials: HTTPBasicCredentials = Depends(security)):
     """ This function is used to log a user with username and password """
     try:
-        user = db.query(models.User).filter(models.User.username == payload.username).first()
-        return {"username": user.username, "email": user.email}
+        token = jwt.encode({"username": credentials.username,
+                            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                           config()["AUTH"]["secret_key"],
+                           algorithm="HS256")
+        return {"token": token}
     except Exception:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"User with username {payload.username} does not exist")
