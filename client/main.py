ee85daf6117c76d014f0eeec2a30c5c6c2e87cfc
pantech48 <evgeniy.putrin@gmail.com>
2023-03-15 16:23:40 +0100

    * main loop

--- client/main.py
+++ client/main.py
@@ -2,4 +2,14 @@ import requests
 from threading import Thread
 
 from utils import sign_up, sign_in, change_password, list_dir
+from logs.logger import logger
 
+
+def main():
+    """Main loop for client"""
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    main()
